74b4885886683e4f2747f0c6e6bf0046259ccab8
Vini Falavigna <vini.df@gmail.com>
2021-07-10 22:44:32 +1000

    [TEST] add react testing library

--- src/components/Hello/Hello.test.tsx
+++ src/components/Hello/Hello.test.tsx
@@ -1,31 +1,12 @@
 import React from "react"
-import { render, unmountComponentAtNode } from "react-dom"
-import { act } from "react-dom/test-utils"
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
 import { Hello } from "."
 
-let container = null
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div")
-  document.body.appendChild(container)
-})
-
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container)
-  container.remove()
-  container = null
-})
-
-it("renders with or without a name", () => {
-  act(() => {
-    render(<Hello compiler="TS" framework="React" />, container)
+describe("Compiler and framework props", () => {
+  it('renders the text correctly', () => {
+    render(<Hello compiler="TS" framework="React" />)
+    expect(screen.findByText('Hello from TS and React')).toBeTruthy()
   })
-  expect(container.textContent).toBe("Hello from TS and React!")
-
-  act(() => {
-    render(<Hello compiler="Babel" framework="Jest" />, container)
-  })
-  expect(container.textContent).toBe("Hello from Babel and Jest!")
 })
